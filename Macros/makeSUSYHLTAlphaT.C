cfefe9c21077569b34c0ad08cec93a2ba705a0f1
MarkBaber <mark.baber@cern.ch>
2014-10-29 10:04:32 +0000

    Seperated AlphaT functions, changed dynamic AlphaT function

--- Macros/makeSUSYHLTAlphaT.C
+++ Macros/makeSUSYHLTAlphaT.C
@@ -43,7 +43,9 @@ const double PB_TO_CM2      = 1e-36;
 #include <iostream>
 #include <math.h>
 
+#include "/home/hep/mb1512/SUSY/UCTHLT/CMSSW_7_2_0_pre8/src/AlphaTHLT/Macros/AlphaT.h"
 #include "/home/hep/mb1512/SUSY/UCTHLT/CMSSW_7_2_0_pre8/src/AlphaTHLT/Macros/JetMatch.h"
+
 
 // Function prototypes
 //TH2D * makeCumu(TH2D * input);
@@ -61,19 +63,21 @@ bool passesAlphaTSelection( std::vector<float> *jetPT, int &jetsAbove50 );
 
 
 
+
 float calculateAlphaT( std::vector<float> *jetPT, std::vector<float> *jetPx, std::vector<float> *jetPy, float jetThreshold );
-float calculateDynamicAlphaT(std::vector<float> *jetPT, std::vector<float> *jetPx, 
-			     std::vector<float> *jetPy, //float jetThreshold, 
-			     uint maxJets, 
+float calculateDynamicAlphaT(std::vector<float> *jetPT, std::vector<float> *jetPx,
+			     std::vector<float> *jetPy, //float jetThreshold,
+			     uint maxJets,
 			     float dynamicJetThreshold, float dynamicAlphaTThreshold);
-std::pair<float,float> calculateDynamicAlphaTHT(std::vector<float> *jetPT, std::vector<float> *jetPx, 
-						std::vector<float> *jetPy, //float jetThreshold, 
+std::pair<float,float> calculateDynamicAlphaTHT(std::vector<float> *jetPT, std::vector<float> *jetPx,
+						std::vector<float> *jetPy, //float jetThreshold,
 						uint maxJets,
                                                 float dynamicJetThreshold, float dynamicAlphaTThreshold, float dynamicHTThreshold);
 
-std::vector<std::pair<float,float> > calculateDynamicAlphaTPairs(std::vector<float> *jetPT, std::vector<float> *jetPx, 
-								 std::vector<float> *jetPy, //float jetThreshold, 
+std::vector<std::pair<float,float> > calculateDynamicAlphaTPairs(std::vector<float> *jetPT, std::vector<float> *jetPx,
+								 std::vector<float> *jetPy, //float jetThreshold,
 								 uint maxJets, float dynamicJetThreshold);
+
 
 
 //float const  PI        = TMath::Pi();
@@ -1006,10 +1010,6 @@ void makeSUSYHLTAlphaT(){
 
   } // End second jet cut
 
-  
-  // Dynamic rates
-  TH2D *NoL1AlphaTDyn_vs_HT_Rate  = new TH2D("NoL1AlphaTDyn_vs_HT_Rate", "No L1 #alpha_{T}^{Dynamic} vs H_{T};H_{T} (GeV);#alpha_{T}^{Dynamic}",  HTBins,HTMin,HTMax,  alphaTBins,alphaTMin,alphaTMax);
-  TH2D *Trig1AlphaTDyn_vs_HT_Rate = new TH2D("Trig1AlphaTDyn_vs_HT_Rate","Trig1 #alpha_{T}^{Dynamic} vs H_{T};H_{T} (GeV);#alpha_{T}^{Dynamic}",  HTBins,HTMin,HTMax,  alphaTBins,alphaTMin,alphaTMax);
 
 
   // ************************************************************************************************************************
@@ -1379,8 +1379,6 @@ void makeSUSYHLTAlphaT(){
 						  maxCaloJet, caloJetDynThreshold, 
     						  caloJetAlphaThreshold );
     
-				uint maxJets,
-				float dynamicJetThreshold, float dynamicAlphaTThreshold){
 
 
 
@@ -1799,8 +1797,6 @@ void makeSUSYHLTAlphaT(){
 
 
 
-dynamicRate noL1DynamicAlphaT    = dynamicRate( HTBins,HTMin,HTMax, alphaTBins,alphaTMin,alphaTMax );
-dynamicRate trig1DynamicAlphaT   = dynamicRate( HTBins,HTMin,HTMax, alphaTBins,alphaTMin,alphaTMax );
 
 #ifdef NEUTRINO
 
@@ -1924,23 +1920,6 @@ dynamicRate trig1DynamicAlphaT   = dynamicRate( HTBins,HTMin,HTMax, alphaTBins,a
 
 
 
-    // Dynamic alphaT rate
-    // calculate all possible alphaT, HT combinations
-    std::vector<std::pair<float,float> > alphaTHTPairs;
-    alphaTHTPairs = calculateDynamicAlphaTPairs(caloJetPT, caloJetPx, caloJetPy, //caloJetThreshold, 
-						maxCaloJet, caloJetDynThreshold );
-
-    for (uint iPair = 0;iPair < alphaTHTPairs.size();++iPair){
-      // x = HT, y = AlphaT
-      noL1DynamicAlphaT.triggerFired(   caloHT,                      alphaTHTPairs[iPair].first );
-      if (l1Trig1){
-	trig1DynamicAlphaT.triggerFired(   caloHT,                      alphaTHTPairs[iPair].first );
-      }
-    }
-    
-    // Calculate trigger rate for event
-    noL1DynamicAlphaT.endEvent();
-    trig1DynamicAlphaT.endEvent();
 
 
 
@@ -2428,29 +2407,6 @@ dynamicRate trig1DynamicAlphaT   = dynamicRate( HTBins,HTMin,HTMax, alphaTBins,a
   // fOut->mkdir("Raw/Correlations");
   // fOut->mkdir("Raw/Distributions");
 
-  fOut->mkdir("Raw/Dynamic");
-
-
-
-  // Dynamic rates
-  std::vector< std::vector< int > > noL1DynAlphaTRate   = noL1DynamicAlphaT.cumulativeSum;
-  std::vector< std::vector< int > > trig1DynAlphaTRate  = trig1DynamicAlphaT.cumulativeSum;
-  
-  for (int xBin = 0; xBin < noL1DynamicAlphaT.xBins; ++xBin ){
-    for (int yBin = 0; yBin < noL1DynamicAlphaT.yBins; ++yBin ){
-      float rateDynAlphaT   = rateScaleFactor * noL1DynAlphaTRate[xBin][yBin];
-      NoL1AlphaTDyn_vs_HT_Rate->SetBinContent( xBin+1, yBin+1  , rateDynAlphaT );
-    }
-  }
-  for (int xBin = 0; xBin < trig1DynamicAlphaT.xBins; ++xBin ){
-    for (int yBin = 0; yBin < trig1DynamicAlphaT.yBins; ++yBin ){
-      float rateDynAlphaT   = rateScaleFactor * trig1DynAlphaTRate[xBin][yBin];
-      Trig1AlphaTDyn_vs_HT_Rate->SetBinContent( xBin+1, yBin+1  , rateDynAlphaT );
-    }
-  }
-  fOut->cd("Raw/Dynamic");
-  NoL1AlphaTDyn_vs_HT_Rate->Write();
-  Trig1AlphaTDyn_vs_HT_Rate->Write();
 
 
 
@@ -2972,196 +2928,6 @@ inline bool passesAlphaTFullSelection( std::vector<float> *jetPT, float HT, floa
 
 
 
-
-
-
-
-// Returns all HT alphaT pairs
-std::vector<std::pair<float,float> > calculateDynamicAlphaTPairs(std::vector<float> *jetPT, std::vector<float> *jetPx, 
-								 std::vector<float> *jetPy,// float jetThreshold, 
-								 uint maxJets, float dynamicJetThreshold){
-
-  float currentHT(0);
-  std::vector<float> jetPTNew;
-  std::vector<float> jetPxNew;
-  std::vector<float> jetPyNew;
-  //  std::vector<float> jetPhiNew;
-  std::vector<std::pair<float,float> > alphaTHTPairs;
-
-  uint jetLimit = std::min( (uint)jetPT->size(), maxJets );
-
-  for(uint iJet = 0;iJet < jetLimit; ++iJet ){
-
-    if( (*jetPT)[iJet] < dynamicJetThreshold ) break;
-
-    // Add more jets to the alphaT collection
-    jetPTNew .push_back( (*jetPT)[iJet] );
-    jetPyNew .push_back( (*jetPx)[iJet] );
-    jetPxNew .push_back( (*jetPy)[iJet] );
-    //    jetPhiNew.push_back( (*jetPhi)[iJet] );
-    currentHT += (*jetPT)[iJet];
-
-    //    if ( iJet > 0 ){
-      float aT = calculateAlphaT( &jetPTNew, &jetPxNew, &jetPyNew, 0 );
-      alphaTHTPairs.push_back( std::make_pair( aT, currentHT ) );
-      //    }
-  }
-
-  return alphaTHTPairs;
-}
-
-
-
-
-
-std::pair<float,float> calculateDynamicAlphaTHT(std::vector<float> *jetPT, std::vector<float> *jetPx, std::vector<float> *jetPy,//float jetThreshold,
- 						uint maxJets, 
-						float dynamicJetThreshold, float dynamicAlphaTThreshold, float dynamicHTThreshold){
-  
-  float maxAlphaT(-1);
-  float currentHT(0);
-  
-  std::vector<float> jetPTNew;
-  std::vector<float> jetPxNew;
-  std::vector<float> jetPyNew;
-  //  std::vector<float> jetPhiNew;
-
-
-  uint jetLimit = std::min( (uint)jetPT->size(), maxJets );
-
-  for(uint iJet = 0;iJet < jetLimit; ++iJet ){
-
-    //    if( std::abs(ijet->eta()) > etaJet_.at(0) ) continue;
-    if( (*jetPT)[iJet] < dynamicJetThreshold ) break;
-
-    // Add more jets to the alphaT collection
-    jetPTNew .push_back( (*jetPT)[iJet] );
-    jetPyNew .push_back( (*jetPx)[iJet] );
-    jetPxNew .push_back( (*jetPy)[iJet] );
-    //    jetPhiNew.push_back( (*jetPhi)[iJet] );
-    currentHT += (*jetPT)[iJet];
-
-    //    if ( iJet > 0 ){
-      float aT = calculateAlphaT( &jetPTNew, &jetPxNew, &jetPyNew, 0 );
-      if ( aT > maxAlphaT ){ maxAlphaT = aT; }
-      if ( (maxAlphaT > dynamicAlphaTThreshold) && (currentHT > dynamicHTThreshold) ){
-	return std::make_pair( maxAlphaT, currentHT );
-      }
-      //    }
-  }
-
-  return std::make_pair( -1, 0 );
-}
-
-
-// Ported from https://github.com/cms-sw/cmssw/blob/CMSSW_7_2_X/HLTrigger/JetMET/src/HLTAlphaTFilter.cc
-float calculateDynamicAlphaT(std::vector<float> *jetPT, std::vector<float> *jetPx, 
-			     std::vector<float> *jetPy, //float jetThreshold, 
-			     uint maxJets, 
-			     float dynamicJetThreshold, float dynamicAlphaTThreshold){
-
-  float maxAlphaT(-1);
-
-  std::vector<float> jetPTNew;
-  std::vector<float> jetPxNew;
-  std::vector<float> jetPyNew;
-  //  std::vector<float> jetPhiNew;
-
-  uint jetLimit = std::min( (uint)jetPT->size(), maxJets );
-
-  for(uint iJet = 0;iJet < jetLimit; ++iJet ){
-
-    //    if( std::abs(ijet->eta()) > etaJet_.at(0) ) continue;
-    if( (*jetPT)[iJet] < dynamicJetThreshold ) break;
-
-    // Add more jets to the alphaT collection
-    jetPTNew .push_back( (*jetPT)[iJet] );
-    jetPxNew .push_back( (*jetPx)[iJet] );
-    jetPyNew .push_back( (*jetPy)[iJet] );
-    //    jetPhiNew.push_back( (*jetPhi)[iJet] );
-    
-    //    if ( iJet > 0 ){
-      float aT = calculateAlphaT( &jetPTNew, &jetPxNew, &jetPyNew, 0 );
-      if ( aT > maxAlphaT ){ maxAlphaT = aT;}
-      if ( maxAlphaT > dynamicAlphaTThreshold ){ break; }
-    }
-  //  }
-
-  return maxAlphaT;
-}
-
-
-
-float calculateAlphaT( std::vector<float> *jetPT, std::vector<float> *jetPx, std::vector<float> *jetPy, float jetThreshold ){
-
-  // check the size of the input collection
-  if (jetPT->size() <= 1){
-    // empty jet collection, return AlphaT = 0
-    return 0.;
-  }
-//   if (jetPT->size() == 1){ // Monojet!
-//     return 10.;
-//   }
-
-//   if (jetPT->size() > (unsigned int) std::numeric_limits<unsigned int>::digits){
-//     // too many jets, return AlphaT = a very large number
-//     return std::numeric_limits<double>::max();
-//   }
-  
-  // Momentum sums in transverse plane
-  float sum_et(0), sum_px(0), sum_py(0);
-
-  // Jet collection restricted to jet threshold
-  std::vector<float> jetPTNew;
-  
-  for (unsigned int iJet = 0; iJet < jetPT->size(); ++iJet ){
-
-    if ( (*jetPT)[iJet] < jetThreshold ){ break; }
-    jetPTNew.push_back( (*jetPT)[iJet] );
-
-    sum_et += (*jetPT)[iJet];    
-    sum_px += (*jetPx)[iJet];
-    sum_py += (*jetPy)[iJet];
-    
-  }
-
-  // check the size of the new input collection
-  if (jetPTNew.size() <= 1){
-    // empty jet collection, return AlphaT = 0
-    return 0.;
-  }
-//   if (jetPTNew.size() == 1){ // Monojet!
-//     return 10.;
-//   }
-
-  // Minimum Delta Et for two pseudo-jets
-  double min_delta_sum_et = sum_et;
-  
-  for (unsigned int i = 0; i < (1U << (jetPTNew.size() - 1)); i++) { //@@ iterate through different combinations
-    double delta_sum_et = 0.;
-    
-    for (unsigned int j = 0; j < jetPTNew.size(); ++j) { //@@ iterate through jets
-      if (i & (1U << j))
-	delta_sum_et -= jetPTNew[j];
-      else
-	delta_sum_et += jetPTNew[j];
-    }
-    
-    delta_sum_et = std::abs(delta_sum_et);
-    if (delta_sum_et < min_delta_sum_et) {
-	min_delta_sum_et = delta_sum_et;
-    }
-    
-  }
-  
-  // Return a large value of alphaT
-  if ( (sum_et*sum_et - (sum_px*sum_px+sum_py*sum_py)) <= 0 ){ 
-    return 10.;
-  }
-
-  // Alpha_T
-  return (0.5 * (sum_et - min_delta_sum_et) / sqrt( sum_et*sum_et - (sum_px*sum_px+sum_py*sum_py) ));  
-}
 
 
 
