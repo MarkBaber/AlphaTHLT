be30dce33bf6fec69a2aabfb7722d37374502334
MarkBaber <mark.baber@cern.ch>
2015-05-26 10:10:42 +0200

    Additional CRAB configs

---
+++ ReRunHLT/crab/crab3_PU30bx50_PHYS14.py
@@ -0,0 +1,51 @@
+from WMCore.Configuration import Configuration
+
+prodTag = "26May15"
+sampleN = 2   # 0 - 10
+jobName = "PHY1474_STV4_742_PU30bx50"
+
+
+datasets = ['/QCD_Pt_30to50_Tune4C_13TeV_pythia8/Phys14DR-AVE30BX50_tsg_castor_PHYS14_ST_V1-v2/GEN-SIM-RAW',
+            '/QCD_Pt_50to80_Tune4C_13TeV_pythia8/Phys14DR-AVE30BX50_tsg_castor_PHYS14_ST_V1-v1/GEN-SIM-RAW',
+            '/QCD_Pt_80to120_Tune4C_13TeV_pythia8/Phys14DR-AVE30BX50_tsg_castor_PHYS14_ST_V1-v1/GEN-SIM-RAW',
+            '/QCD_Pt_120to170_Tune4C_13TeV_pythia8/Phys14DR-AVE30BX50_tsg_castor_PHYS14_ST_V1-v1/GEN-SIM-RAW',
+            '/QCD_Pt_170to300_Tune4C_13TeV_pythia8/Phys14DR-AVE30BX50_tsg_castor_PHYS14_ST_V1-v1/GEN-SIM-RAW',
+            '/QCD_Pt_300to470_Tune4C_13TeV_pythia8/Phys14DR-AVE30BX50_tsg_castor_PHYS14_ST_V1-v1/GEN-SIM-RAW',
+            '/QCD_Pt_470to600_Tune4C_13TeV_pythia8/Phys14DR-AVE30BX50_tsg_castor_PHYS14_ST_V1-v1/GEN-SIM-RAW',
+            '/QCD_Pt_600to800_Tune4C_13TeV_pythia8/Phys14DR-AVE30BX50_tsg_castor_PHYS14_ST_V1-v1/GEN-SIM-RAW',
+            '/QCD_Pt_800to1000_Tune4C_13TeV_pythia8/Phys14DR-AVE30BX50_tsg_castor_PHYS14_ST_V1-v1/GEN-SIM-RAW',
+            ]
+labels   = ['QCD30to50',
+            'QCD50to80',
+            'QCD80to120',
+            'QCD120to170',
+            'QCD170to300',
+            'QCD300to470',
+            'QCD470to600',
+            'QCD600to800',
+            'QCD800to1000',
+            ]
+
+dataset = datasets[ sampleN ]
+label   = labels[ sampleN ]
+
+config = Configuration()
+config.section_('General')
+config.General.workArea = jobName + '_' + prodTag
+config.General.transferOutputs = True
+config.General.requestName = label
+config.section_('JobType')
+config.JobType.psetName = '../test/AlphaT_HLT_742V3_PHYS14_50ns.py'
+config.JobType.pluginName = 'Analysis'
+config.JobType.outputFiles = ['hltReRunResults.root']
+config.section_('Data')
+config.Data.outLFNDirBase   = '/store/user/mbaber/' + jobName + '_' + prodTag
+config.Data.inputDBS        = 'global'
+config.Data.inputDataset = dataset
+config.Data.publication  = False
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 5
+#config.Data.totalUnits = 1
+config.section_('User')
+config.section_('Site')
+config.Site.storageSite = 'T2_UK_London_IC'
---
+++ ReRunHLT/crab/crab3_PU30bx50_Spring15.py
@@ -0,0 +1,51 @@
+from WMCore.Configuration import Configuration
+
+prodTag = "24May15"
+sampleN = 10   # 0 - 10
+jobName = "FALL1374_25V4_742_PU40bx25_HCAL3"
+
+
+datasets = ['/QCD_Pt_30to50_TuneCUETP8M1_13TeV_pythia8/RunIISpring15Digi74-AVE_30_BX_50ns_tsg_MCRUN2_74_V6-v1/GEN-SIM-RAW',
+            '/QCD_Pt_50to80_TuneCUETP8M1_13TeV_pythia8/RunIISpring15Digi74-AVE_30_BX_50ns_tsg_MCRUN2_74_V6-v1/GEN-SIM-RAW',
+            '/QCD_Pt_80to120_TuneCUETP8M1_13TeV_pythia8/RunIISpring15Digi74-AVE_30_BX_50ns_tsg_MCRUN2_74_V6-v1/GEN-SIM-RAW',
+            '/QCD_Pt_120to170_TuneCUETP8M1_13TeV_pythia8/RunIISpring15Digi74-AVE_30_BX_50ns_tsg_MCRUN2_74_V6-v1/GEN-SIM-RAW',
+            '/QCD_Pt_170to300_TuneCUETP8M1_13TeV_pythia8/RunIISpring15Digi74-AVE_30_BX_50ns_tsg_MCRUN2_74_V6-v1/GEN-SIM-RAW',
+            '/QCD_Pt_300to470_TuneCUETP8M1_13TeV_pythia8/RunIISpring15Digi74-AVE_30_BX_50ns_tsg_MCRUN2_74_V6-v1/GEN-SIM-RAW',
+            '/QCD_Pt_470to600_TuneCUETP8M1_13TeV_pythia8/RunIISpring15Digi74-AVE_30_BX_50ns_tsg_MCRUN2_74_V6-v1/GEN-SIM-RAW',
+            '/QCD_Pt_600to800_TuneCUETP8M1_13TeV_pythia8/RunIISpring15Digi74-AVE_30_BX_50ns_tsg_MCRUN2_74_V6-v1/GEN-SIM-RAW',
+            '/QCD_Pt_800to1000_TuneCUETP8M1_13TeV_pythia8/RunIISpring15Digi74-AVE_30_BX_50ns_tsg_MCRUN2_74_V6-v1/GEN-SIM-RAW',
+            ]
+labels   = ['QCD30to50',
+            'QCD50to80',
+            'QCD80to120',
+            'QCD120to170',
+            'QCD170to300',
+            'QCD300to470',
+            'QCD470to600',
+            'QCD600to800',
+            'QCD800to1000',
+            ]
+
+dataset = datasets[ sampleN ]
+label   = labels[ sampleN ]
+
+config = Configuration()
+config.section_('General')
+config.General.workArea = jobName + '_' + prodTag
+config.General.transferOutputs = True
+config.General.requestName = label
+config.section_('JobType')
+config.JobType.psetName = '../test/AlphaT_HLT_742V2_25ns.py'
+config.JobType.pluginName = 'Analysis'
+config.JobType.outputFiles = ['hltReRunResults.root']
+config.section_('Data')
+config.Data.outLFNDirBase   = '/store/user/mbaber/' + jobName + '_' + prodTag
+config.Data.inputDBS        = 'global'
+config.Data.inputDataset = dataset
+config.Data.publication  = False
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 5
+#config.Data.totalUnits = 1
+config.section_('User')
+config.section_('Site')
+config.Site.storageSite = 'T2_UK_London_IC'
