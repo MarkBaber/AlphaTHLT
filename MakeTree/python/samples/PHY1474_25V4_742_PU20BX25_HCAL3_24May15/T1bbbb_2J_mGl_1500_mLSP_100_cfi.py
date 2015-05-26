22006f19b8e9c2ecdfccd88aa849183c34081cc8
MarkBaber <mark.baber@cern.ch>
2015-05-26 11:19:44 +0100

    Latest sample lists

---
+++ MakeTree/python/samples/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/T1bbbb_2J_mGl_1500_mLSP_100_cfi.py
@@ -0,0 +1,41 @@
+import FWCore.ParameterSet.Config as cms
+# ---------------------------------------------------------------------------------------------------------------------------------------
+# T1bbbb_2J_mGl_1500_mLSP_100
+# ---------------------------------------------------------------------------------------------------------------------------------------
+T1bbbb_2J_mGl_1500_mLSP_100 = cms.PSet(
+	name  = cms.string("SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola.root"),
+	files = cms.untracked.vstring()
+)
+T1bbbb_2J_mGl_1500_mLSP_100.files.extend([
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_27.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_15.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_26.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_12.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_7.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_23.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_14.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_3.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_24.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_25.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_5.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_8.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_13.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_6.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_22.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_28.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_21.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_19.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_16.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_18.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_11.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_17.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_20.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_10.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_2.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_9.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_4.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T1bbbb_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T1bbbb_2J_mGl_1500_mLSP_100/150524_184734/0000/hltReRunResults_1.root',
+])
+
+# ---------------------------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------------------------
---
+++ MakeTree/python/samples/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/T2qq_2J_mStop_1200_mLSP_100_cfi.py
@@ -0,0 +1,29 @@
+import FWCore.ParameterSet.Config as cms
+# ---------------------------------------------------------------------------------------------------------------------------------------
+# T2qq_2J_mStop_1200_mLSP_100
+# ---------------------------------------------------------------------------------------------------------------------------------------
+T2qq_2J_mStop_1200_mLSP_100 = cms.PSet(
+	name  = cms.string("SMS-T2qq_2J_mStop-1200_mLSP-100_Tune4C_13TeV-madgraph-tauola.root"),
+	files = cms.untracked.vstring()
+)
+T2qq_2J_mStop_1200_mLSP_100.files.extend([
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T2qq_2J_mStop-1200_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T2qq_2J_mStop_1200_mLSP_100/150524_185245/0000/hltReRunResults_14.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T2qq_2J_mStop-1200_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T2qq_2J_mStop_1200_mLSP_100/150524_185245/0000/hltReRunResults_16.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T2qq_2J_mStop-1200_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T2qq_2J_mStop_1200_mLSP_100/150524_185245/0000/hltReRunResults_10.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T2qq_2J_mStop-1200_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T2qq_2J_mStop_1200_mLSP_100/150524_185245/0000/hltReRunResults_3.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T2qq_2J_mStop-1200_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T2qq_2J_mStop_1200_mLSP_100/150524_185245/0000/hltReRunResults_4.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T2qq_2J_mStop-1200_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T2qq_2J_mStop_1200_mLSP_100/150524_185245/0000/hltReRunResults_7.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T2qq_2J_mStop-1200_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T2qq_2J_mStop_1200_mLSP_100/150524_185245/0000/hltReRunResults_13.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T2qq_2J_mStop-1200_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T2qq_2J_mStop_1200_mLSP_100/150524_185245/0000/hltReRunResults_6.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T2qq_2J_mStop-1200_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T2qq_2J_mStop_1200_mLSP_100/150524_185245/0000/hltReRunResults_1.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T2qq_2J_mStop-1200_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T2qq_2J_mStop_1200_mLSP_100/150524_185245/0000/hltReRunResults_9.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T2qq_2J_mStop-1200_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T2qq_2J_mStop_1200_mLSP_100/150524_185245/0000/hltReRunResults_11.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T2qq_2J_mStop-1200_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T2qq_2J_mStop_1200_mLSP_100/150524_185245/0000/hltReRunResults_2.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T2qq_2J_mStop-1200_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T2qq_2J_mStop_1200_mLSP_100/150524_185245/0000/hltReRunResults_8.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T2qq_2J_mStop-1200_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T2qq_2J_mStop_1200_mLSP_100/150524_185245/0000/hltReRunResults_12.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T2qq_2J_mStop-1200_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T2qq_2J_mStop_1200_mLSP_100/150524_185245/0000/hltReRunResults_15.root',
+	'root://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/mbaber/PHY1474_25V4_742_PU20BX25_HCAL3_24May15/SMS-T2qq_2J_mStop-1200_mLSP-100_Tune4C_13TeV-madgraph-tauola/crab_T2qq_2J_mStop_1200_mLSP_100/150524_185245/0000/hltReRunResults_5.root',
+])
+
+# ---------------------------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------------------------
