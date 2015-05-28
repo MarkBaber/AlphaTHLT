8a09b858e6639921bed5f1b9fbe7c25c3c6bca34
MarkBaber <mark.baber@cern.ch>
2015-05-28 14:07:38 +0200

    Added PU20bx25 PHYS14 sample list

---
+++ ReRunHLT/crab/crab3_PU20bx25_PHYS14.py
@@ -0,0 +1,51 @@
+from WMCore.Configuration import Configuration
+
+prodTag = "28May15"
+sampleN =  6  # 0 - 8
+jobName = "PHY1474_STV4_742_PU20bx25"
+
+
+datasets = ['/QCD_Pt-30to50_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v2/GEN-SIM-RAW',
+            '/QCD_Pt-50to80_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW',
+            '/QCD_Pt-80to120_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW',
+            '/QCD_Pt-120to170_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW',
+            '/QCD_Pt-170to300_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW',
+            '/QCD_Pt-300to470_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW',
+            '/QCD_Pt-470to600_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW',
+            '/QCD_Pt-600to800_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW',
+            '/QCD_Pt-800to1000_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW',
+            ]
+labels   = ['QCD30to50',
+            'QCD50to80',
+            'QCD80to120',
+            'QCD120to170',
+            'QCD170to300',
+            'QCD300to470',
+            'QCD470to600',
+            'QCD600to800',
+            'QCD800to1000',
+            ]
+
+dataset = datasets[ sampleN ]
+label   = labels[ sampleN ]
+
+config = Configuration()
+config.section_('General')
+config.General.workArea = jobName + '_' + prodTag
+config.General.transferOutputs = True
+config.General.requestName = label
+config.section_('JobType')
+config.JobType.psetName = '../test/AlphaT_HLT_742V2_25ns_PHYS14.py'
+config.JobType.pluginName = 'Analysis'
+config.JobType.outputFiles = ['hltReRunResults.root']
+config.section_('Data')
+config.Data.outLFNDirBase   = '/store/user/mbaber/' + jobName + '_' + prodTag
+config.Data.inputDBS        = 'global'
+config.Data.inputDataset = dataset
+config.Data.publication  = False
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 5
+#config.Data.totalUnits = 1
+config.section_('User')
+config.section_('Site')
+config.Site.storageSite = 'T2_UK_London_IC'
