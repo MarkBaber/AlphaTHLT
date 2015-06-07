c156ba5056c98fdbedf456fd0d050afb1ebc16d3
MarkBaber <mark.baber@cern.ch>
2015-06-07 21:12:54 +0200

    Added data-like trigger samples

--- ReRunHLT/crab/crab3_PU20bx25_PHYS14.py
+++ ReRunHLT/crab/crab3_PU20bx25_PHYS14.py
@@ -1,21 +1,31 @@
 from WMCore.Configuration import Configuration
+from collections import OrderedDict
 
-prodTag = "28May15"
-sampleN =  6  # 0 - 8
+prodTag = "07Jun15"
+sampleN =  0  # 0 - 8
 jobName = "PHY1474_STV4_742_PU20bx25"
 signal  = True
+datasets= {}
 
-datasets = {}
-datasets = {'QCD30to50'   :'/QCD_Pt-30to50_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v2/GEN-SIM-RAW',  
-            'QCD50to80'   :'/QCD_Pt-50to80_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW',  
-            'QCD80to120'  :'/QCD_Pt-80to120_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW', 
-            'QCD120to170' :'/QCD_Pt-120to170_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW',
-            'QCD170to300' :'/QCD_Pt-170to300_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW',
-            'QCD300to470' :'/QCD_Pt-300to470_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW',
-            'QCD470to600' :'/QCD_Pt-470to600_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW',
-            'QCD600to800' :'/QCD_Pt-600to800_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW',
-            'QCD800to1000':'/QCD_Pt-800to1000_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW',
-            }
+if signal == True:
+        datasets = {'WToMuNu'   :'/WToMuNu_Tune4C_13TeV-pythia8/Phys14DR-AVE20BX25_tsg_PHYS14_25_V3-v1/GEN-SIM-RAW',
+                    'DYToMuMu'  :'/DYToMuMu_Tune4C_13TeV-pythia8/Phys14DR-AVE20BX25_tsg_PHYS14_25_V3-v1/GEN-SIM-RAW',
+                    # 'WToENu'   :'/WToENu_Tune4C_13TeV-pythia8/Phys14DR-AVE20BX25_tsg_PHYS14_25_V3-v1/GEN-SIM-RAW',
+                    # 'DYToEE'  :'/DYToEE_Tune4C_13TeV-pythia8/Phys14DR-AVE20BX25_tsg_PHYS14_25_V3-v1/GEN-SIM-RAW',
+                    }
+
+else:
+
+    datasets = {'QCD30to50'   :'/QCD_Pt-30to50_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v2/GEN-SIM-RAW',  
+                'QCD50to80'   :'/QCD_Pt-50to80_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW',  
+                'QCD80to120'  :'/QCD_Pt-80to120_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW', 
+                'QCD120to170' :'/QCD_Pt-120to170_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW',
+                'QCD170to300' :'/QCD_Pt-170to300_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW',
+                'QCD300to470' :'/QCD_Pt-300to470_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW',
+                'QCD470to600' :'/QCD_Pt-470to600_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW',
+                'QCD600to800' :'/QCD_Pt-600to800_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW',
+                'QCD800to1000':'/QCD_Pt-800to1000_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW',
+                }
 
 dataset = datasets.values()[ sampleN ]
 label   = datasets.keys()  [ sampleN ]
